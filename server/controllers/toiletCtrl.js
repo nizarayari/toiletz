c1ffa4e8a7cf9f8f037ff235e6b2eefdf928587b
Justin Chen <justinpchen94@gmail.com>
2016-07-07 10:50:29 -0700

    [SETUP] added toilet endpoints to controller (#8)
    * [SETUP] added review endpoints to controller
    
    * [SETUP] added toilet endpoints to controller

--- server/controllers/toiletCtrl.js
+++ server/controllers/toiletCtrl.js
@@ -16,5 +16,23 @@ module.exports = {
       console.log("Received DELETE at /api/toilet/");
       res.end("Received DELETE at /api/toilet/");
     }
+  },
+  '/:toiletId': {
+    get: function(req,res) {
+      console.log("Received GET at /api/:toiletId");
+      res.end("Received GET at /api/:toiletId");
+    },
+    post: function(req, res) {
+      console.log("Received POST at /api/:toiletId");
+      res.end("Received POST at /api/:toiletId");
+    },
+    put: function(req, res) {
+      console.log("Received PUT at /api/:toiletId");
+      res.end("Received PUT at /api/:toiletId");
+    },
+    delete: function(req, res) {
+      console.log("Received DELETE at /api/:toiletId");
+      res.end("Received DELETE at /api/:toiletId");
+    }
   }
 };
