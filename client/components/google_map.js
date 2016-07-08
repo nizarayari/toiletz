15c3cf45b839c8d3ef89a5aedec20706947342d9
Nizar Ayari <nizarayari17@gmail.com>
2016-07-08 11:49:19 -0700

    [FEAT] add google maps component

--- client/components/google_map.js
+++ client/components/google_map.js
@@ -1,16 +1,77 @@
 import React from 'react';
-import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
+import {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
 
-export default (props) => {
+const coords = {
+  lat: 34.020440,
+  lng: -118.494297
+};
 
-	return(
-		<GoogleMapLoader
-			containerElement={ <div style={{height:'100%'}} /> }
-			googleMapElement={
-				<GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}}
-				/>
-			}
-		/>
+const image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
 
-	);
-}
+export default class SimpleMap extends Component{
+
+  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: true,
+      streetViewControl: true,
+      streetViewControlOptions:true,
+      zoomControl:true,
+      zoomControlOptions: true,
+      panControl:true,
+      panControlOptions: true
+    });
+  }
+
+  onDragEnd(e) {
+    console.log('onDragEnd', e);
+  }
+
+  onCloseClick() {
+    console.log('onCloseClick');
+  }
+
+  onClick(e) {
+    console.log('onClick', e);
+  }
+
+  render() {
+    return (
+      <Gmaps
+        width={'1200px'}
+        height={'600px'}
+        lat={coords.lat}
+        lng={coords.lng}
+        zoom={14}
+        loadingMessage={'Be happy'}
+        params={{v: '3.exp', key: 'AIzaSyB85KqmtnH-PdxoaFTRZRWZJLI6H48oa-Q'}}
+        onMapCreated={this.onMapCreated}>
+        <Marker
+          lat={coords.lat}
+          lng={coords.lng}
+          draggable={true}
+          onDragEnd={this.onDragEnd}
+          icon = {image}
+           />
+           <Marker
+          lat={34.016484}
+          lng={-118.496216}
+          draggable={true}
+          onDragEnd={this.onDragEnd} />
+        <InfoWindow
+          lat={coords.lat}
+          lng={coords.lng}
+          content={'Hello, MKS-41:)'}
+          onCloseClick={this.onCloseClick} />
+        <Circle
+          lat={coords.lat}
+          lng={coords.lng}
+          radius={100}
+          onClick={this.onClick} />
+      </Gmaps>
+    );
+  }
+
+};
+
