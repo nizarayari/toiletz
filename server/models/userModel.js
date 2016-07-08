25eb30e9310012141f874da797b6d06c0754dbf6
Justin Chen <justinpchen94@gmail.com>
2016-07-08 15:37:37 -0700

    [FEAT] implemented sign in and sign up api calls, can create users and hash their pw's and compare them as well

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,39 +3,57 @@ var bcrypt = require('bcrypt');
 
 var User = module.exports;
 
-User.createUser = function(attr) {
-
-  console.log("inserting new user: ", attr);
-
-  return hashPassword(attr.password)
-    .then(function(hashObj) {
-      attr.password = hashObj.hash;
-      attr.salt = hashObj.salt;
-    })
-    .then(function(){
-      return db('users').insert(attr)
-        .then(function(result) {
-          attr.id = result[0];
-          delete attrs.password;
-          return attr;
-        });    
+User.findUserByUsername = function(username) {
+  return db('users').where({ username: username }).limit(1)
+    .then(function (rows) {
+      return rows[0];
     });
+};
 
+User.findUserById = function(id) {
+  return db('users').where({ id: id }).limit(1)
+    .then(function (rows) {
+      return rows[0];
+    });
+};
+
+User.findUserByEmail = function(email) {
+  return db('users').where({ email: email }).limit(1)
+    .then(function (rows) {
+      return rows[0];
+    });
+};
+
+User.createUser = function(attr) {
+  return new Promise(function(resolve, reject) {
+    return hashPassword(attr.password)
+      .then(function(hashObj) {
+        attr.password = hashObj.hash;
+        attr.salt = hashObj.salt;
+      })
+      .then(function(){
+        return db('users').insert(attr)
+          .then(function(result) {
+            attr.id = result[0];
+            delete attr.password;
+            resolve(attr);
+          });    
+      });
+  });
+  
 };
 
 function hashPassword(pw) {
+  console.log("hashing password");
   return new Promise(function(resolve, reject) {
-
     return bcrypt.genSalt(10, function(err, salt) {
       if (err) {
         return next(err);
       }
-
       bcrypt.hash(pw, salt, function(err, hash) {
         if (err) {
           return next(err);
         }
-
         resolve({
           salt: salt,
           hash: hash
@@ -43,7 +61,15 @@ function hashPassword(pw) {
       });
 
     });
-
   });
   
+}
+
+User.comparePasswords = function(hashedPw, attempt) {
+  return new Promise(function(resolve, reject) {
+    bcrypt.compare(attempt, hashedPw, function(err, res) {
+      if (err) return reject(err);
+      resolve(res);
+    });
+  });
 }
