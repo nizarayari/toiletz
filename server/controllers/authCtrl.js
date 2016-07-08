eba889ff6dac878a1cfb332838999a53532039f1
Justin Chen <justinpchen94@gmail.com>
2016-07-08 15:37:37 -0700

    [FEAT] implemented sign in and sign up api calls, can create users and hash their pw's and compare them as well

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -1,3 +1,6 @@
+var User = require('../models/UserModel.js');
+var Q = require('q');
+
 module.exports = {
   'signin': {
     get: function(req,res) {
@@ -6,6 +9,31 @@ module.exports = {
     },
     post: function(req, res) {
       console.log("Received POST at /api/auth/signin");
+
+      var password = 'password';
+      var email = 'examples@email.com';
+      User.findUserByEmail(email)
+        .then(function(user) {
+          if (user) {
+            console.log("user exists, checking pw");
+            User.comparePasswords(user.password, password)
+              .then(function(res) {
+                if (res) {
+                  console.log("passwords match");
+                  //proceed with login methods
+                } else {
+                  console.log("passwords do not match");
+                  //throw error and have front end display warning
+                }
+              });
+          } else {
+            console.log("user does not exist");
+            //redirect to signup page
+          }
+        });
+
+
+
       res.end("Received POST at /api/auth/signin");
     },
     put: function(req, res) {
@@ -24,7 +52,32 @@ module.exports = {
     },
     post: function(req, res) {
       console.log("Received POST at /api/auth/signup");
-      res.end("Received POST at /api/auth/signup");
+      
+      var email = 'examples@email.com';
+
+      User.findUserByEmail(email)
+        .then(function(user) {      
+          if (user) {
+            console.log("user exists");
+            res.end("Cannot create user; user already exists");
+          } else {
+            console.log("user does not exist");
+            User.createUser({
+              username: 'test',
+              password: 'password',
+              email: 'examples@email.com',
+              gender: 'male'
+            }).then(function(result) {
+              console.log("result", result);
+
+              //do session id/jwt stuff
+              res.end("Received POST at /api/auth/signup");
+            });
+          }
+
+        });
+      
+      
     },
     put: function(req, res) {
       console.log("Received PUT at /api/auth/signup");
