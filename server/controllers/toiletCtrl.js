cbb15a84feb02822486d0b6b99a6a0c856258555
Justin Chen <justinpchen94@gmail.com>
2016-07-08 17:32:03 -0700

    [FEAT] can add toilets and look them up by location or id

--- server/controllers/toiletCtrl.js
+++ server/controllers/toiletCtrl.js
@@ -1,3 +1,6 @@
+var Toilet = require('../models/toiletModel.js');
+var User = require('../models/userModel.js');
+
 module.exports = {
   '/': {
     get: function(req,res) {
@@ -6,6 +9,33 @@ module.exports = {
     },
     post: function(req, res) {
       console.log("Received POST at /api/toilet/");
+      console.log("creating toilet");
+
+      //need user id as an input
+
+      var newToilet = {
+        description: "a new toilet",
+        id_Users: 15,
+        location: 'santa monica'
+
+      };
+
+      Toilet.findToiletByLocation(newToilet.location)
+        .then(function(toilet) {
+          if (toilet) {
+            console.log("toilet already added"); 
+          } else {
+            console.log("toilet does not exist");
+
+            Toilet.createToilet(newToilet)
+              .then(function(result) {
+                console.log("result", result);
+
+                res.end(result);
+              });
+          }
+        });
+
       res.end("Received POST at /api/toilet/");
     },
     put: function(req, res) {
@@ -19,20 +49,20 @@ module.exports = {
   },
   ':toiletId': {
     get: function(req,res) {
-      console.log("Received GET at /api/:toiletId");
-      res.end("Received GET at /api/:toiletId");
+      console.log("Received GET at /api/toilet/:toiletId");
+      res.end("Received GET at /api/toilet/:toiletId");
     },
     post: function(req, res) {
-      console.log("Received POST at /api/:toiletId");
-      res.end("Received POST at /api/:toiletId");
+      console.log("Received POST at /api/toilet/:toiletId");
+      res.end("Received POST at /api/toilet/:toiletId");
     },
     put: function(req, res) {
-      console.log("Received PUT at /api/:toiletId");
-      res.end("Received PUT at /api/:toiletId");
+      console.log("Received PUT at /api/toilet/:toiletId");
+      res.end("Received PUT at /api/toilet/:toiletId");
     },
     delete: function(req, res) {
-      console.log("Received DELETE at /api/:toiletId");
-      res.end("Received DELETE at /api/:toiletId");
+      console.log("Received DELETE at /api/toilet/:toiletId");
+      res.end("Received DELETE at /api/toilet/:toiletId");
     }
   }
 };
