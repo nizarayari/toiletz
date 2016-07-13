de99856aa05b63da481bac42d8baeb22092a8063
MKS-students <mks.students@hackreactor.com>
2016-07-12 17:22:28 -0700

    [TEST] Worked on details page

--- src/container/toilet_details.js
+++ src/container/toilet_details.js
@@ -13,17 +13,30 @@ class ToiletDetail extends Component{
 			<div>
 				<h3>Details for:</h3>
 				<div>{this.props.toilet.description}</div>
-				<div>{this.props.toilet.description}</div>
+
+				<div>{this.props.toilet.reviews.map((review, index) => {
+					return (
+							<li>{review}</li>
+							)
+						}
+					)
+				}
+				</div>
 			</div>
 		);
 	}
 }
 
 function mapStateToProps(state){
-	return{
+	return {
 		toilet:state.activeToilet //from rootReducer (index.js in reducers)
 	}
 }
-
+//   connect reducers and containers here
 export default connect (mapStateToProps)(ToiletDetail);
 
+//reviews
+//description
+//rating
+//name
+//address
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+*import { combineReducers } from 'redux';
 import SearchReducer from './reducer_search';
 import ToiletReducer from './reducer_toilets';
 import ActiveToilet from './reducer_active_toilets';
