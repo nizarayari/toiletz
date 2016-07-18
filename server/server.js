af3b6e490ab98c0f87447a893e02af1bfdbd6ddf
Justin Chen <justinpchen94@gmail.com>
2016-07-18 16:31:31 -0700

    [BUG] fixing dots again

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
-app.use(express.static('./src'));
+app.use(express.static('../src'));
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
