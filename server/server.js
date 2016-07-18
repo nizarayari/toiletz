cb707839742cc2000cf7167168af061183cac0b7
Justin Chen <justinpchen94@gmail.com>
2016-07-18 11:01:51 -0700

    [FEAT] added jwt middleware for routes

--- server/server.js
+++ server/server.js
@@ -22,6 +22,37 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
+// route middleware to verify a token
+var tokenCheck = function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, process.env.secret, function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+};
+
 app.use(express.static('../src'));
 
 app.use(function(req, res, next) {
@@ -32,11 +63,11 @@ app.use(function(req, res, next) {
 });
 
 //Routing
-app.use('/api/review', routesReview);
-app.use('/api/user', routesUser);
-app.use('/api/toilet', routesToilet);
+app.use('/api/review', tokenCheck, routesReview);
+app.use('/api/user', tokenCheck, routesUser);
+app.use('/api/toilet', tokenCheck, routesToilet);
 app.use('/api/auth', routesAuth);
-app.use('/api/tag', routesTag);
+app.use('/api/tag', tokenCheck, routesTag);
 
 app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, '../src', 'index.html'));
